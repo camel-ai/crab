8c0974cd2a16f24279c2f81a9b33689d5a898bae
Isaac Jin <whale3ye@gmail.com>
2024-08-11 20:24:38 -0500

    camel integration WIP

--- crab/agents/backend_models/camel_model.py
+++ crab/agents/backend_models/camel_model.py
@@ -11,23 +11,54 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # =========== Copyright 2024 @ CAMEL-AI.org. All Rights Reserved. ===========
-from typing import Any, Dict, Optional
+import base64
+import io
+import json
+from typing import Any, Dict, List, Optional, Tuple, Union
 
-from crab import BackendModel
+from PIL import Image
+
+from crab import Action, ActionOutput, BackendModel, BackendOutput, MessageType
 
 try:
     from camel.agents import ChatAgent
+    from camel.messages import BaseMessage
     from camel.models import ModelFactory
+    from camel.toolkits import OpenAIFunction
+    from camel.types.enums import ModelPlatformType, ModelType
 
     CAMEL_ENABLED = True
 except ImportError:
     CAMEL_ENABLED = False
 
 
+def find_model_platform_type(model_platform_name: str) -> ModelPlatformType:
+    for platform in ModelPlatformType:
+        if platform.value.lower() == model_platform_name.lower():
+            return platform
+    all_models = [platform.value for platform in ModelPlatformType]
+    raise ValueError(
+        f"Model {model_platform_name} not found. Supported models are {all_models}"
+    )
+
+
+def find_model_type(model_name: str) -> Union[ModelType, str]:
+    for model in ModelType:
+        if model.value.lower() == model_name.lower():
+            return model
+    return model_name
+
+
+def decode_image(encoded_image: str) -> Image:
+    data = base64.b64decode(encoded_image)
+    return Image.open(io.BytesIO(data))
+
+
 class CamelModel(BackendModel):
     def __init__(
         self,
         model: str,
+        model_platform: str,
         parameters: Optional[Dict[str, Any]] = None,
         history_messages_len: int = 0,
     ) -> None:
@@ -39,14 +70,79 @@ class CamelModel(BackendModel):
             parameters,
             history_messages_len,
         )
-        # model = ModelFactory.create(model, **parameters)
-        # self._client = ChatAgent(model)
+        # TODO: a better way?
+        model_type = find_model_type(model)
+        model_platform_type = find_model_platform_type(model_platform)
+
+        # TODO: model_config_dict can't be None?
+        self.camel_model = ModelFactory.create(
+            model_platform_type, model_type, model_config_dict=parameters or {}
+        )
+
+        self.client: Optional[ChatAgent] = None
+        self.token_usage = 0
+        self.action_schema: Optional[List[OpenAIFunction]] = None
 
     def get_token_usage(self):
-        pass
+        return self.token_usage
 
-    def reset(self, system_message, action_space):
-        pass
+    def reset(self, system_message: str, action_space: Optional[List[Action]]) -> None:
+        sysmsg = BaseMessage.make_assistant_message(
+            role_name="Assistant",
+            content=system_message,
+        )
+        self.action_schema = self._convert_action_to_schema(action_space)
+        self.client = ChatAgent(
+            model=self.camel_model,
+            system_message=sysmsg,
+            tools=self.action_schema,
+        )
+        self.token_usage = 0
 
-    def chat(self, message):
-        pass
+    # TODO: convert Action into OpenAIFunction
+    @staticmethod
+    def _convert_action_to_schema(
+        action_space: Optional[List[Action]],
+    ) -> Optional[List[OpenAIFunction]]:
+        if action_space is None:
+            return None
+        return [OpenAIFunction(action.entry) for action in action_space]
+
+    @staticmethod
+    def _convert_tool_calls_to_action_list(tool_calls) -> List[ActionOutput]:
+        if tool_calls is None:
+            return tool_calls
+        return [
+            ActionOutput(
+                name=call.function.name,
+                arguments=json.loads(call.function.arguments),
+            )
+            for call in tool_calls
+        ]
+
+    def chat(self, messages: List[Tuple[str, MessageType]]):
+        # TODO: handle multiple text messages?
+        image_list: List[Image] = []
+        content = ""
+        for message in messages:
+            if message[1] == MessageType.IMAGE_JPG_BASE64:
+                image = decode_image(message[0])
+                image_list.append(image)
+            else:
+                content = message[0]
+        usermsg = BaseMessage.make_user_message(
+            role_name="User",
+            content=content,
+            image_list=image_list,
+        )
+        response = self.client.step(usermsg)
+        # TODO: which one indicates usage in one response?
+        self.token_usage += response.info["usage"]["total_tokens"]
+
+        # TODO: can delete this after record_message is refactored
+        self.client.record_message(response.msg)
+
+        # TODO: function calls will not be exposed in CAMEL
+        return BackendOutput(
+            message=response.msg.content,
+        )
