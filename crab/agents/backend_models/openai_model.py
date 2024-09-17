f0d5f6b26902f1e2f85ad4b9f1da8f6ef6016a75
Tianqi Xu <tianqi.xu@kaust.edu.sa>
2024-09-17 20:41:32 +0300

    Tiny fix

--- crab/agents/backend_models/openai_model.py
+++ crab/agents/backend_models/openai_model.py
@@ -124,7 +124,7 @@ class OpenAIModel(BackendModel):
                         {
                             "type": "image_url",
                             "image_url": {
-                                "url": f"data:image/jpeg;base64,{message[0]}",
+                                "url": f"data:image/jpeg;base64,{content}",
                                 "detail": "high",
                             },
                         }
